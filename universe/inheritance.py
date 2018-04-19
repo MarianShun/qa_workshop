22c81f981fba67a1ce59ee5635d378a0591d6b23
MarianShun <37655860+MarianShun@users.noreply.github.com>
2018-04-19 12:54:19 +0300

    Update inheritance.py

--- universe/inheritance.py
+++ universe/inheritance.py
@@ -1,42 +1,85 @@
-car_capacity = 4
+import pytest
+from inheritance import Car
+from random import randint
+from inheritance import Star
+from inheritance import Planet
 
-class SpaceObject():
-    def __init__(self, name, m, ro, o_o, r):
-                
-        if not str(name.replace(' ', '')).isalnum():
-            raise NameError('Name is not valid!')
-        self.name = str(name)
-        self.m = float(m)
-        self.ro = ro
-        self.o_o = self.validate_orbiting(o_o)  
-        self.r = r 
-        self.population = 0
-        
-    def validate_orbiting(self, o_o):
-        if type(o_o) in (list, tuple, set):
-            return o_o
-        else:
-            raise TypeError('Orbiting Objects should be iterable!')
-        
-    def send_peeps_here(self, addnl):
-        self.population = self.population + addnl
-            
-            
-class Car(SpaceObject):
-    def send_peeps_here(self, addnl):
-        if self.population + addnl <= 0:
-            self.population = 0
-        elif self.population + addnl >= car_capacity:
-            self.population = car_capacity
-        elif self.population + addnl < car_capacity:
-            self.population = self.population + addnl
+class TestsTesla:
+    def test_tesla_popltn1(self):
+        tesla = Car('Roadster', 2,3,[], 6)
+        tesla.send_peeps_here(1)
+        assert tesla.population == 1
+    
+    @pytest.mark.mark1
+    @pytest.mark.mark2
+    def test_tesla_popltn2(self):
+        tesla = Car('Roadster', 2,3,[], 6)
+        tesla.send_peeps_here(2)
+        assert tesla.population == 2, 'Wrong population!!!111'
+    
+    @pytest.mark.mark1
+    def test_tesla_popltn_dot5(self):
+        tesla = Car('Roadster', 2,3,[], 6)
+        tesla.send_peeps_here(.5)
+        assert tesla.population == .5
+    @pytest.mark.mark2
+    def test_tesla_popltn_1000(self):
+        tesla = Car('Roadster', 2,3,[], 6)
+        tesla.send_peeps_here(1000)
+        assert tesla.population == 2
 
-class Star(SpaceObject):
-    def send_peeps_here(self, addnl):
-            self.population = 0
-            
-class Planet(SpaceObject):
-    def __init__(self, name, m, ro, o_o, r, atm):
-        super().__init__(name, m, ro, o_o, r)
-        self.atm = atm
         
+@pytest.mark.parametrize('mass', [randint(0,1000) for x in range(5)])
+@pytest.mark.parametrize('density', [randint(0,1000) for y in range(5)])
+@pytest.mark.parametrize('radius', [randint(0,1000) for y in range(5)])
+class TestsParametrizedCar:
+    names = ['Roadster', 'Model S', 'SpaceX']  
+    @pytest.mark.parametrize(('name_expected'), names)
+    def test_car_name_positive(self, name_expected, mass, density, radius):
+        #name_expected = 'Roadster'
+        tesla = Car(name_expected, mass, density,[], radius)
+        assert tesla.name == name_expected
+        
+    def test_car_name_whitespace_negative(self, mass, density, radius):
+        name_not_alnum = 'Tesla-Roadster'
+        with pytest.raises(NameError):
+            Car(name_not_alnum, mass, density,[], radius)
+			
+@pytest.mark.parametrize('n', [randint(0,1000) for x in range(10)])
+class TestsStarPopulation:
+    def test_star_popltn(self, n):
+        stars = Star('Star1', 2,3,[], 6)
+        stars.send_peeps_here(n)
+        assert stars.population == 0
+			
+@pytest.mark.parametrize('mass', [randint(0,9000000) for x in range(5)])
+@pytest.mark.parametrize('density', [randint(0,9000000) for y in range(5)])
+@pytest.mark.parametrize('radius', [randint(0,9000000) for y in range(5)])
+class TestsParametrizedStar:
+    names = ['LHS 2924', 'Проксіма Центавра', 'Альфа Центавра A', 'Альфа Центавра B','Сіріус А', 'Сіріус B', 'Мю Цефея', 'Зірка Барнарда', 'Зірка у Пістолеті', 'HD189733b','М31','NGC 224']  
+    @pytest.mark.parametrize(('name_expected'), names)
+    def test_star_name_positive(self, name_expected, mass, density, radius):
+        stars = Star(name_expected, mass, density,[], radius)
+        assert stars.name == name_expected
+    
+    names_not_alnum = ['','LHS-2924', 'Проксіма.Центавра', 'Альфа/Центавра А', 'Альфа Центавра\'B','Сіріус,А', 'Сіріус;B', 'Мю\Цефея', 'Зірка=Барнарда', 'Зірка[уПістолеті', 'HD189733{b','М_31','NG(C224']  
+    @pytest.mark.parametrize(('name_not_alnum'), names_not_alnum)
+    def test_star_name_negative(self, name_not_alnum, mass, density, radius):
+        with pytest.raises(NameError):
+            Star(name_not_alnum, mass, density,[], radius)
+			
+@pytest.mark.parametrize('mass', [randint(0,100000) for x in range(5)])
+@pytest.mark.parametrize('density', [randint(0,100000) for y in range(5)])
+@pytest.mark.parametrize('radius', [randint(0,100000) for y in range(5)])
+class TestsParametrizedPlanet:
+    names = ['Аврора','Альфа','Анакреонт','Арктур','Асконь','Асперта','Баронн','Бонда','Ванда','Вега','Венкорі','Вінсеторі','Ворег','Гамма Андромеда','Гелікон','Геторин','Гесперос','Гея','Гліпталь IV', 'HD1897332b','М321','NGC 2324']  
+    @pytest.mark.parametrize(('name_expected'), names)
+    def test_planet_name_positive(self, name_expected, mass, density, radius):
+        planets = Planet(name_expected, mass, density,[], radius, 1)
+        assert planets.name == name_expected
+    
+    names_not_alnum = ['','Авр-ора','Аль.фа','Анак\реонт','А/рктур','Аскон,ь','Ас;перта','Барон=н','Б[онда','В{анда','В_ега','Венк(орі','Вінс`еторі','Во\'рег','Гамма"Андромеда']
+    @pytest.mark.parametrize(('name_not_alnum'), names_not_alnum)
+    def test_planet_name_negative(self, name_not_alnum, mass, density, radius):
+        with pytest.raises(NameError):
+            Planet(name_not_alnum, mass, density,[], radius, 1)
